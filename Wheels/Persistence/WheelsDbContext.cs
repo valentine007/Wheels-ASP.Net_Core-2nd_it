04941aa12bad0257030496e4f4d16a161b131f76
Valentyn Demchuk <vdemchuk.vd@gmail.com>
2018-08-02 22:25:11 +0300

    automapper

---
+++ Wheels/Persistence/WheelsDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Wheels.Models;
+
+namespace Wheels.Persistence
+{
+    public class WheelsDbContext : DbContext
+    {
+		public WheelsDbContext(DbContextOptions<WheelsDbContext> options) : base(options)
+		{
+
+		}
+
+		public DbSet<Make> Makes { get; set; }
+    }
+}
